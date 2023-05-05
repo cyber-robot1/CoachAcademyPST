8d5068446fdc4e1fa724ba2aebbef87cb8101090
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-05-05 18:46:19 +0300

    Create Discount Fare.cpp

---
+++ Level 1/Sheet 1 (Introduction to Programming)/Discount Fare.cpp
@@ -0,0 +1,20 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Discount Fare
+Problem link: https://atcoder.jp/contests/abc113/tasks/abc113_a
+Date: 4/5/2023
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+	int X, Y;
+	cin >> X >> Y;
+	Y /= 2;
+	cout << X + Y << endl;
+
+	return 0;
+}

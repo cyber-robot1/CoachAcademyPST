7b6bff8690aa54f666104a9729a4493b864bd3a2
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-05-05 18:51:16 +0300

    Create Domino piling.cpp

---
+++ Level 1/Sheet 1 (Introduction to Programming)/Domino piling.cpp
@@ -0,0 +1,25 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Domino piling
+Problem link: https://codeforces.com/problemset/problem/50/A
+Date: 4/5/2023
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int M, N;
+	cin >> M >> N;
+	cout << (M * N) / 2 << endl;
+
+	return 0;
+}
+
+
+
+
+
+

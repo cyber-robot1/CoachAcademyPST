5c3c6216dcdf7e1f1ad24b013c983cd9370d8f2c
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-05-05 18:36:30 +0300

    Create Brick.cpp

---
+++ Level 1/Sheet 1 (Introduction to Programming)/Brick.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Brick
+Problem link: https://atcoder.jp/contests/abc186/tasks/abc186_a
+Date: 4/5/2023
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	int N, W;
+	cin >> N >> W;
+	cout << N / W;
+  
+  return 0;
+}

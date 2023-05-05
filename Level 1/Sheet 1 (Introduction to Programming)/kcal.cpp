61c99b444896b6be0273b9f15536e3345bff4149
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-05-05 18:50:07 +0300

    Create kcal.cpp

---
+++ Level 1/Sheet 1 (Introduction to Programming)/kcal.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: kcal
+Problem link: https://atcoder.jp/contests/abc205/tasks/abc205_a
+Date: 4/5/2023
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int A, B;
+	cin >> A >> B;
+	cout << (double)A * (double)B / 100;
+
+	return 0;
+}

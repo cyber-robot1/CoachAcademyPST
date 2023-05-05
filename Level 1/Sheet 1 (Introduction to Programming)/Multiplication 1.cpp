784dae7ff7487ae3afffd2a992f066fd914f3f97
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-05-05 18:40:11 +0300

    Create Multiplication 1.cpp

---
+++ Level 1/Sheet 1 (Introduction to Programming)/Multiplication 1.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Multiplication 1
+Problem link: https://atcoder.jp/contests/abc169/tasks/abc169_a
+Date: 4/5/2023
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	 
+    int A, B;
+	  cin >> A >> B;
+	  cout << A * B << endl;
+  
+    return 0;
+}

4c5adf4f69528249015cf5ad1e1575e9429d59ec
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-05-05 18:47:44 +0300

    Create New Year.cpp

---
+++ Level 1/Sheet 1 (Introduction to Programming)/New Year.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: New Year
+Problem link: https://atcoder.jp/contests/abc084/tasks/abc084_a
+Date: 4/5/2023
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int M;
+	cin >> M;
+	cout << 24 + (24 - M) << endl;
+
+	return 0;
+}

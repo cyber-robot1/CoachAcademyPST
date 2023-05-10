c6aedc91275b0ed5ddabaa0e5c02a4185925d450
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-05-10 15:34:20 +0300

    Create Wizards' Duel.cpp

---
+++ Level 1/Sheet 1 (Introduction to Programming)/Wizards' Duel.cpp
@@ -0,0 +1,18 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Wizards' Duel
+Problem link: https://codeforces.com/problemset/problem/591/A
+Date: 10/5/2023
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double l, p, q;
+	cin >> l >> p >> q;
+	cout << (p * l) / (p + q);
+
+	return 0;
+}

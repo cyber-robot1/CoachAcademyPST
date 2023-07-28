a9de429fe53743eee840a46aa4ff534e71a7089f
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-07-28 19:42:49 +0300

    Create Chores.cpp

---
+++ Level 1/Sheet 6 (Function & Built-In Functions)/Chores.cpp
@@ -0,0 +1,39 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Chores
+Problem link: https://codeforces.com/problemset/problem/837/A
+Date: 28/7/2023
+*/
+
+
+#include <iostream>
+using namespace std;
+ 
+int arr[101];
+int main()
+{
+	
+	int n, k, x, sum = 0;
+	cin >> n >> k >> x;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+ 
+	for (int i = n - 1; k != 0; i--, k--)
+	{
+		sum = sum + x;
+		arr[i] = 0;
+	}
+ 
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == 0)
+			break;
+		else
+			sum = sum + arr[i];
+	}
+	
+	cout << sum;
+ 
+ 
+	return 0;
+}

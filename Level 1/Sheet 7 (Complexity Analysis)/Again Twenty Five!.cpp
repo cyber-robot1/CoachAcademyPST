610d1b819b132839ff103bbbc72e762b47c85b3d
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-08-06 13:38:32 +0300

    Create Again Twenty Five!.cpp

---
+++ Level 1/Sheet 7 (Complexity Analysis)/Again Twenty Five!.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Again Twenty Five!
+Problem link: https://codeforces.com/problemset/problem/630/A
+Date: 6/8/2023
+*/
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	
+	long long n;
+	cin >> n;
+	cout << 25;
+ 
+	return 0;
+}
